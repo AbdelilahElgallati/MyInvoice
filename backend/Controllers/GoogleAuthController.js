8b550312d9b6501ac4d8226772ac344d5e43c0e0
hamzajaada <hamzajaada22@gmail.com>
2024-05-11 19:57:41 +0200

    push by hamza

--- backend/Controllers/GoogleAuthController.js
+++ backend/Controllers/GoogleAuthController.js
@@ -13,8 +13,7 @@ module.exports = (passport) => {
 
     passport.use(
         new GoogleStrategy({
-            clientID: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            
             callbackURL: "http://localhost:3001/auth/google/callback",
     
         }, async function(accessToken, refreshToken, profile, cb) {
