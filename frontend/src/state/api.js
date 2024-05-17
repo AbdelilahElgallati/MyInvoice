680ccdd5c67249cfdb120204db1b8ea1b7f25643
AbdelilahElgallati <abdelilah.elgallati6@gamil.com>
2024-05-17 23:11:52 +0100

    commit by abdelilah

--- frontend/src/state/api.js
+++ frontend/src/state/api.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: "https://my-invoice-api.vercel.app/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://my-invoice-api.vercel.app" }),
   reducerPath: "adminApi",
   tagTypes: [
     "Entreprise",
