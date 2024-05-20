4f9739d2a0a7a1a9dcd43b765f53c7e847f485c4
AbdelilahElgallati <abdelilah.elgallati6@gamil.com>
2024-05-20 16:10:47 +0100

    commit by abdelilah

--- frontend/src/pages/Pack/AddPack.jsx
+++ frontend/src/pages/Pack/AddPack.jsx
@@ -1,15 +1,27 @@
 import React, { useState } from "react";
-import { TextField, useTheme, Button, Box, FormControl, InputLabel, Select,Typography,  MenuItem, Chip, Input } from "@mui/material";
+import {
+  TextField,
+  useTheme,
+  Button,
+  Box,
+  FormControl,
+  InputLabel,
+  Select,
+  Typography,
+  MenuItem,
+  Chip,
+  Input,
+} from "@mui/material";
 import Header from "componentsAdmin/Header";
-import {  useGetAllServicesQuery, useAddPackMutation } from "state/api";
+import { useGetAllServicesQuery, useAddPackMutation } from "state/api";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const AddPack = () => {
-  const navigate = useNavigate()
-  if(!localStorage.getItem('userId')) {
-    navigate('/');
+  const navigate = useNavigate();
+  if (!localStorage.getItem("userId")) {
+    navigate("/");
   }
   const theme = useTheme();
   const [logo, setLogo] = useState([]);
@@ -37,43 +49,45 @@ const AddPack = () => {
     };
   };
 
-  
   const handleSubmit = async (event) => {
     event.preventDefault();
+    const formattedServices = services.map((serviceId) => ({ serviceId }));
     const pack = {
       name,
       description,
       price,
-      services: services.map(service => service.serviceId || service),
+      services: formattedServices,
       logo,
-    }
+    };
     try {
       const { data } = await addPack(pack);
       if (data.success) {
-        toast.success("L'enregistrement de pack se passe correctement");
+        toast.success("L'enregistrement de pack s'est bien passé");
         navigate("/packadmin");
       } else {
         toast.error(
-          "L'enregistrement de pack ne s'est pas passé correctement : " + data.message
+          "L'enregistrement de pack ne s'est pas passé correctement : " +
+            data.message
         );
-        console.log(data)
+        console.log(data.error);
       }
-      
     } catch (error) {
       toast.error("Erreur lors de l'ajoute de pack : " + error.message);
       console.log(error);
     }
   };
 
-
   return (
     <Box m="1.5rem 2.5rem">
       <Header title="ADD PACK" subtitle="Ajout d'un nouveau pack" />
-      <form onSubmit={handleSubmit} sx={{
-        backgroundImage: "none",
-        backgroundColor: theme.palette.background.alt,
-        borderRadius: "0.55rem",
-      }} >
+      <form
+        onSubmit={handleSubmit}
+        sx={{
+          backgroundImage: "none",
+          backgroundColor: theme.palette.background.alt,
+          borderRadius: "0.55rem",
+        }}
+      >
         <TextField
           label="Nom de pack"
           name="name"
@@ -102,7 +116,7 @@ const AddPack = () => {
           required
           margin="normal"
         />
-        <FormControl fullWidth margin="normal">
+        {/* <FormControl fullWidth margin="normal">
           <InputLabel id="services-label">Services</InputLabel>
           <Select
             labelId="services-label"
@@ -125,9 +139,41 @@ const AddPack = () => {
               </MenuItem>
             ))}
           </Select>
+        </FormControl> */}
+        <FormControl fullWidth margin="normal">
+          <InputLabel id="services-label">Services</InputLabel>
+          <Select
+            labelId="services-label"
+            id="services"
+            multiple
+            value={services}
+            onChange={handleServiceChange}
+            renderValue={(selected) => (
+              <div style={{ display: "flex", flexWrap: "wrap" }}>
+                {selected.map((serviceId) => {
+                  const service = servicesData.find(
+                    (service) => service._id === serviceId
+                  );
+                  return <Chip key={serviceId} label={service?.ServiceName} />;
+                })}
+              </div>
+            )}
+          >
+            {servicesData &&
+              servicesData.map((service) => (
+                <MenuItem key={service._id} value={service._id}>
+                  {service.ServiceName}
+                </MenuItem>
+              ))}
+          </Select>
         </FormControl>
         <FormControl fullWidth margin="normal">
-          <Typography variant="body1" component="label" htmlFor="icon-input" sx={{ display: 'block', marginBottom: '0.5rem' }}>
+          <Typography
+            variant="body1"
+            component="label"
+            htmlFor="icon-input"
+            sx={{ display: "block", marginBottom: "0.5rem" }}
+          >
             Icon
           </Typography>
           <Input
@@ -137,11 +183,11 @@ const AddPack = () => {
             onChange={handleImage}
             accept="image/*"
             sx={{
-              display: 'block',
-              padding: '10px 14px',
+              display: "block",
+              padding: "10px 14px",
               border: `1px solid ${theme.palette.divider}`,
-              borderRadius: '4px',
-              cursor: 'pointer',
+              borderRadius: "4px",
+              cursor: "pointer",
             }}
           />
         </FormControl>
