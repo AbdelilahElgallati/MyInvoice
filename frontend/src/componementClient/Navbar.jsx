154f108c26cd53f54a5ac0e14e88825fc6e10dcb
AbdelilahElgallati <abdelilah.elgallati6@gamil.com>
2024-05-20 18:45:37 +0100

    commit by abdelilah

--- frontend/src/componementClient/Navbar.jsx
+++ frontend/src/componementClient/Navbar.jsx
@@ -93,7 +93,7 @@ const Navbar = ({ user, isSidebarOpen, setIsSidebarOpen }) => {
               <Box
                 component="img"
                 alt="profile"
-                src={user.logo ? (user.logo.startsWith('http') ? user.logo : `https://my-invoice-api.vercel.app/Images/${user.logo}`) : "#"}
+                src={user.logo.url}
                 height="32px"
                 width="32px"
                 borderRadius="50%"
