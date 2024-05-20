c627100d194f999f738c1b1803350321e6c01139
AbdelilahElgallati <abdelilah.elgallati6@gamil.com>
2024-05-20 18:57:15 +0100

    commit by abdelilah

--- frontend/src/componementClient/Navbar.jsx
+++ frontend/src/componementClient/Navbar.jsx
@@ -93,7 +93,7 @@ const Navbar = ({ user, isSidebarOpen, setIsSidebarOpen }) => {
               <Box
                 component="img"
                 alt="profile"
-                src={user.logo.url}
+                src={user && user.logo && user.logo.url}
                 height="32px"
                 width="32px"
                 borderRadius="50%"
